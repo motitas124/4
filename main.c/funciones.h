6f0bd5b60ad9d2906c543881fde235cbbbb5a5e8
miniduck <andres.mendez.cazares@udla.edu.ec>
2024-06-19 12:07:16 -0500

    taller

---
+++ main.c/funciones.h
@@ -0,0 +1,7 @@
+void ingresarCliente(char clientes[5][2][40]);
+void imprimirClientes(char clientes[5][2][40]);
+void listarPeliculas(char peliculas[10][4][40]);
+void buscarporNombre(char peliculas[10][4][40]);
+void buscarporGenero(char peliculas[10][4][40]);
+void comprarTicket(char peliculas[10][4][40], double precio[3], char clientes[5][2][40], int reserva[10][4]);
+void verCompras(char peliculas[10][4][40], double precio[3], char clientes[5][2][40], int reserva[10][4]);
